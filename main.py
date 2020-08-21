1a5e699e20131292a5638e0d7fceb3f52a836f29
Theodore Hui <tchui@ucsd.edu>
2020-08-21 20:43:02 +0000

    Buggy :(

---
+++ main.py
@@ -0,0 +1,138 @@
+import pygame
+
+import os
+
+ 
+
+# These are the dimensions of the background image for our game
+
+screenLength = 800
+
+screenHeight = 427 
+
+dim_field = (screenLength, screenHeight)
+
+
+
+def main():
+    #------screen set up-----#
+    screen = pygame.display.set_mode(dim_field)
+    clock = pygame.time.Clock()
+    background = pygame.image.load(os.path.join("assets","background.jpg"))
+    background = pygame.transform.scale(background, dim_field)
+
+    #----Player Mechanics----#
+    
+    # Location 1
+    platforms = []
+    rect_player = pygame.Rect(200, 200, 24, 26)
+    rect_platform1 = pygame.Rect(200,250,300,5)
+    rect_platform2 = pygame.Rect(200,180,300,5)
+    platforms.append(rect_platform1)
+    platforms.append(rect_platform2)
+
+
+    FPS = 60
+    stepSize = 12
+    running = True
+    isJumping = False
+    isGrounded = False
+    isFalling = True
+    while running:
+        clock.tick(FPS)
+
+        keys = pygame.key.get_pressed() #list of keys that are being pressed
+        for event in pygame.event.get():
+            # Quit game
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    running = False
+                    print("Quit game")
+                #isJumping
+
+                
+                #Horiz Mvmt
+                #if event.key == pygame.K_d:#move right
+            
+            #while rect_player.left >=0 and rect_player.right <= screenLength:
+            print(keys[pygame.K_d])
+            if keys[pygame.K_d]:
+                rect_player.move_ip(stepSize,0)
+                #if event.key == pygame.K_a:#move left
+            if keys[pygame.K_a]:
+                rect_player.move_ip(-stepSize,0)
+            '''if keys[pygame.K_s]:
+                rect_player.move_ip(0,stepSize)
+                #if event.key == pygame.K_a:#move left
+            if keys[pygame.K_w]:
+                rect_player.move_ip(0,-stepSize)'''
+
+
+            #if isGrounded:
+                #if keys[ pygame.K_SPACE]:
+                    #isJumping = True
+                #isFalling = False
+            while isGrounded:
+                if rect_player.collidelist(platforms) == -1:
+                    isFalling = True
+                    isGrounded = False
+                if keys[ pygame.K_SPACE]:
+                    isJumping = True
+                    isGrounded = False
+            
+            while isFalling:
+                if rect_player.collidelist(platforms) != -1:
+                    isGrounded = True
+                    isFalling = False
+                fall(rect_player, 1)
+            
+            while isJumping:#Player Jumping Mechanics
+                jump(rect_player)
+                isFalling = True
+                isJumping = False
+            '''
+                rect_player.move_ip(0,-10)
+                jumpCounter += 1
+                isGrounded = False
+                if jumpCounter == 10:
+                    isJumping = False
+                    isFalling = True
+                    jumpCounter = 0
+            '''
+        
+                     
+
+            
+        
+        #Setting boundaries
+        if rect_player.left <= 0:
+            rect_player.move_ip(stepSize,0)
+        if rect_player.right >= screenLength:
+            rect_player.move_ip(-stepSize,0)
+        if rect_player.bottom >= screenHeight:
+            rect_player.move_ip(0,-1)
+        if rect_player.top <= 0:
+            rect_player.move_ip(0,1)
+
+
+
+
+        screen.blit(background, (0,0)) 
+        pygame.draw.rect(screen, (255,0,0), rect_player)
+        
+        for a in range(len(platforms)):
+            pygame.draw.rect(screen, (255,255,0), platforms[a])
+        pygame.display.update()
+
+def jump(rect):
+    jumpCounter = 0
+    rect.move_ip(0,-10)
+    jumpCounter += 1
+    if jumpCounter == 10:
+        jumpCounter = 0
+
+def fall(rect,stepsizeY):
+    rect.move_ip(0,stepsizeY)
+
+if __name__ == '__main__':   
+    main()
